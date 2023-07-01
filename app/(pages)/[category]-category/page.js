56064bb9bc17cf613f354fb3fef512d7a8bb4600
davidkartuzinski <david@kaidawei.me>
2023-07-02 00:14:06 +0200

    removed category page with commenting as creating errors

--- app/(pages)/[category]-category/page.js
+++ app/(pages)/[category]-category/page.js
@@ -1,54 +1,49 @@
-import { allPosts } from '../../../.contentlayer/generated';
-import PostLayout from '../../components/server-side/post-layout';
-import dayjs from 'dayjs';
-import { slugify } from '../../lib/helpers';
-
 //https://github.com/vercel/next.js/discussions/41745#discussioncomment-4264660
 
 // @baszalmstra Server components have a params prop that you can use to grab dynamic paths. /protected/[pageNumber] will have ServerComponent({ params: { pageNumber } })
 
-export const dynamicParams = false;
+// export const dynamicParams = false;
 
-export async function getStaticParams() {
-  let paths = [];
+// export async function getStaticParams() {
+//   let paths = [];
 
-  allPosts.map((post) => {
-    if (post.draft === false) {
-      post.categories.map((category) => {
-        const slug = slugify(category);
-        paths.push({ params: { slug } });
-      });
-    }
-  });
+//   allPosts.map((post) => {
+//     if (post.draft === false) {
+//       post.categories.map((category) => {
+//         const slug = slugify(category);
+//         paths.push({ params: { slug } });
+//       });
+//     }
+//   });
 
-  return { paths };
-}
+//   return { paths };
+// }
 
-function fetchPost(params) {
-  const rawPost = allPosts.find(
-    (post) => post._raw.flattenedPath === params.slug
-  );
+// function fetchPost(params) {
+//   const rawPost = allPosts.find(
+//     (post) => post._raw.flattenedPath === params.slug
+//   );
 
-  const post = {
-    ...rawPost,
-    date: dayjs(rawPost.date).format('MMMM D, YYYY'),
-  };
+//   const post = {
+//     ...rawPost,
+//     date: dayjs(rawPost.date).format('MMMM D, YYYY'),
+//   };
 
-  return post;
-}
+//   return post;
+// }
 
-export default function Post({ params }) {
-  const post = fetchPost(params);
+// export default function Post({ params }) {
+//   const post = fetchPost(params);
 
-  return (
-    <PostLayout
-      title={post.title}
-      author={post.author}
-      date={post.date}
-      categories={post.categories}
-      tags={post.tags}
-      readingTime={post.readingTime.text}
-      body={post.body.code}
-    />
-  );
-}
+//   return (
+//     <PostLayout
+//       title={post.title}
+//       author={post.author}
+//       date={post.date}
+//       categories={post.categories}
+//       tags={post.tags}
+//       readingTime={post.readingTime.text}
+//       body={post.body.code}
+//     />
+//   );
+// }
